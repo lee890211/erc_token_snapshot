216c687dbafd7a120731c45a90deb027d469934a
Sangseo Lee <lee890211@hotmail.com>
2018-08-22 01:55:12 +0900

    improved balance accuracy

--- snapshot.py
+++ snapshot.py
@@ -12,6 +12,7 @@ res = json.load(urllib.request.urlopen(url_get_token_addr))
 
 holders_cnt = res['holdersCount']
 num_pages = (holders_cnt+9)//10
+denom = 10 ** int(res['decimals'])
 
 template_url = "https://ethplorer.io/service/service.php?data={0}&page=tab%3Dtab-holders%26holders%3D{1}"
 for i in range(num_pages):
@@ -27,4 +28,4 @@ for i in range(num_pages):
 
     print('----------------------------page {}------------------------------------'.format(page))
     for account in data['holders']:
-        print("account: {0} balance: {1}".format(account['address'], format(account['balance']/10e+17, ",.2f")))
+        print("account: {0} balance: {1}".format(account['address'], format(account['balance']/denom, ",")))
